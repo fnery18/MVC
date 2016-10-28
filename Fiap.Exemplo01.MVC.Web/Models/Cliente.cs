10b605528914ef5e72cbee78d0cc1693e83a9a73
Felipe Nery <felipenerypaulino@gmail.com>
2016-10-28 18:29:44 -0200

    Adicionado Estado Civil/NecessidadesEsp/Data de Nasc

--- Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
+++ Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,15 @@ namespace Fiap.Exemplo01.MVC.Web.Models
         public string nome { get; set; }
         public int idade { get; set; }
         public decimal credito { get; set; }
+
+        [Display(Name = "Possui necessidades especiais?")]
+        public bool NecessidadesEspeciais { get; set; }
+
+       [Display(Name = "Estado Civil")]
+        public string EstadoCivil {get; set;}
+
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
     }
 }
